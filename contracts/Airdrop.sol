dd37d306501ad1ef4cf99fedae7e3d5a719155bb
RyanGuoRL <ruiliang.guo@gmail.com>
2022-05-23 02:48:09 +0800

    Update Airdrop.sol

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -1,33 +1,45 @@
 pragma solidity >=0.7.0;
+pragma abicoder v2;
 import "./Ownable.sol";
 import "./SafeMath.sol";
 import "./Token.sol";
 
 
-contract Airdrop is Ownable {
+contract BatchAirdrop is Ownable {
     using SafeMath for uint;
 
-    address public tokenAddr;
-
-    event EtherTransfer(address beneficiary, uint amount);
+    address [] public tokensAddr;
+    string [] public tokenSymbols;
 
     constructor(address _tokenAddr) public {
-        tokenAddr = _tokenAddr;
+        tokensAddr.push(_tokenAddr);
+        tokenSymbols.push(Token(_tokenAddr).symbol());
     }
 
-    function airdropTokens(address[] memory _recipients, uint256[] memory _amount) public onlyOwner returns (bool) {
-       
+    function airdropTokens(uint tokenIndex, address[] memory _recipients, uint256[] memory _amount) public onlyOwner returns (bool) {
+       require(tokenIndex < tokensAddr.length, "Index out of bound");
         for (uint i = 0; i < _recipients.length; i++) {
             require(_recipients[i] != address(0));
-            require(Token(tokenAddr).transfer(_recipients[i], _amount[i]));
+            require(Token(tokensAddr[tokenIndex]).transfer(_recipients[i], _amount[i]));
         }
-
         return true;
     }
 
+    function addTokenAddr(address[] memory newTokenAddr) public onlyOwner {
+        for (uint i=0; i < newTokenAddr.length; i++) {
+            tokensAddr.push(newTokenAddr[i]);
+            tokenSymbols.push(Token(newTokenAddr[i]).symbol());
+        }
+    }
 
-    function updateTokenAddress(address newTokenAddr) public onlyOwner {
-        tokenAddr = newTokenAddr;
+    function withdrawTokens(uint tokenIndex, address beneficiary) public onlyOwner {
+        require(tokenIndex < tokensAddr.length, "Index out of bound");
+        require(Token(tokensAddr[tokenIndex]).transfer(beneficiary, Token(tokensAddr[tokenIndex]).balanceOf(address(this))));
+    }
+
+    function balanceOfToken(uint tokenIndex) public onlyOwner returns (uint256){
+        require(tokenIndex < tokensAddr.length, "Index out of bound");
+        return Token(tokensAddr[tokenIndex]).balanceOf(address(this));
     }
 
 }
